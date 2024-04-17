9fed4d95a1e6195781c99cbb5b967b68b50a6063
Saurav Kumar <ss989108@gmail.com>
2024-04-17 11:18:10 +0530

    server update

--- backend-server/node-server/index.js
+++ backend-server/node-server/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Hello World');
+app.get('/route-handler', (req, res) =>{
+    res.json({
+        name: "saurav",
+        age: 21
+    })
 }); 
+
+app.get('/', (req, res) =>{
+    res.send("Hello World");
+});
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`);
