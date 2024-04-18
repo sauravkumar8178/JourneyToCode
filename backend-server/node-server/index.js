f60a494f702a6d881bbb11d8a0cc09e2a5aba9d1
Saurav Kumar <ss989108@gmail.com>
2024-04-19 00:14:05 +0530

    server update

--- backend-server/node-server/index.js
+++ backend-server/node-server/index.js
@@ -9,10 +9,15 @@ app.get('/route-handler', (req, res) =>{
     })
 }); 
 
+app.post('/conversation', (res, req) => {
+    console.log(req.Header);
+    res.send('<br>Lets Chat!</br>');
+});
+
 app.get('/', (req, res) =>{
     res.send("Hello World");
 });
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`);
-});
+});     
