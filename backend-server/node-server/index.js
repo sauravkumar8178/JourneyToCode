f6b322411f55a223e83b77e2c95417a7122cd021
Saurav Kumar <ss989108@gmail.com>
2024-05-08 07:38:51 +0530

    add query string and process.env

--- backend-server/node-server/index.js
+++ backend-server/node-server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
+
+// Define the port for the server to listen on.
+// If the PORT environment variable is set, use its value.
+// Otherwise, default to port 3000.
+//Sets the environment variable PORT to 3006 using the export command.
+//export PORT=3006
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.get('/route-handler', (req, res) =>{
@@ -10,10 +17,19 @@ app.get('/route-handler', (req, res) =>{
     });
 }); 
 
-app.use(bodyParser.json()); // we can use express in replace of bodyParser
+app.use(bodyParser.json()); // Parses incoming JSON requests and attaches the parsed data to req.body.
 
 app.post('/backend-api/conversation', (req, res) => {
-    const message = req.body.message;  // To access the client message by using body parser
+    
+    // Extracts the value of the 'message' property from the request body
+    // and assigns it to the variable 'message' for further processing.
+    const message = req.body.message; 
+    
+    // Extracts the value of the 'messageQuery' parameter from the request's query string
+    // and assigns it to the variable 'messageQuery' for further processing.
+    const messageQuery = req.query.messageQuery;
+
+    console.log(messageQuery);
     console.log(message);
     res.json({
         output: "data is sended"
@@ -21,7 +37,7 @@ app.post('/backend-api/conversation', (req, res) => {
 })
 
 app.post('/conversation', (req, res) => { 
-
+    // Logs the content of the request body and the request headers for debugging purposes.
     console.log(req.body);
     console.log(req.headers);
     res.send('<br>Lets Chat!</br>');
