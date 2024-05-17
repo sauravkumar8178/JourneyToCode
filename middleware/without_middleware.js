3b4768135971b88a4bfee1f6236843f6e2a1649c
Saurav Kumar <ss989108@gmail.com>
2024-05-17 07:16:18 +0530

    adding username and password check

--- middleware/without_middleware.js
+++ middleware/without_middleware.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3001
 
+app.use(express.json);
 
+app.get('/health-checkup', (req, res) =>{
+    // do health check here
+    const kidneyId = req.query.kidneyId;
+    const username = req.header.username;
+    const password = req.header.password;
+    if(username != "saurav@gmail.com" && password != "pass"){
+        res.status(403).json({
+            msg: "Username and Password are incorrect. Please try again!"
+        });
+        return;
+    }
+
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg: "Wrong Input!"
+        })
+    }
+
+    res.send("Your heart is healthy");
+})
 
 
 
